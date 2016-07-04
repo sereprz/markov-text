ec5e916fb369d92c8897494b9a147ce74b2810cb
sereprz <sereprz@gmail.com>
2016-07-04 16:32:22 +0200

    added order param

--- MarkovChain.py
+++ MarkovChain.py
@@ -5,42 +5,67 @@ import re
 
 
 class MarkovChain():
-    def __init__(self, raw_text):
+    def __init__(self, raw_text, order):
+
+        self.order = order
 
         sentences = []
-        for line in re.split(re.compile('\.|!|\?|"'), raw_text):
+        for line in re.split(re.compile('\.|!|\?|,"'), raw_text):
             if line.strip() != '':
                 sentences.append(line.strip())
 
         self.text = '$'.join([s[:1].lower() + s[1:] for s in sentences])
 
         tokens = ['$'] + nltk.word_tokenize(self.text)
-        states_freq = Counter(tokens)
 
-        bigrams = list(nltk.bigrams(tokens))
-        trans_freq = Counter(bigrams)
+        states = [' '.join(state) for state in nltk.ngrams(tokens, n=order)]
+        states_freq = Counter(states)
+
+        ngrams = [' '.join(ngram) for ngram in nltk.ngrams(tokens, n=order + 1)]
+        ngrams_freq = Counter(ngrams)
 
         trans_prob = {}
-        for state, freq in trans_freq.items():
-            trans_prob[state] = float(freq)/states_freq[state[0]]
+        for ngram, freq in ngrams_freq.items():
+            state = ' '.join(ngram.split(' ')[:-1])
+            trans_prob[ngram] = float(freq)/states_freq[state]
 
-        self.states_freq = states_freq
-        self.trans_freq = trans_freq
-        self.trans_prob = trans_prob
+        first = [state for state in states_freq if state.split(' ')[0] == '$']
+        p_start = np.array([float(states_freq[state])/len(sentences) for state in first])
 
-    def next_word(self, w):
-        bgs = [bigram for bigram in self.trans_prob if bigram[0] == w]
-        w2 = [bg[1] for bg in bgs]
-        p = np.array([self.trans_prob[bg] for bg in bgs])
-        if p.sum() < 1:
-            p /= p.sum()
+        self.first_state_prob = {'state': first, 'p': p_start}
+        self.transitions = trans_prob
 
-        return np.random.choice(w2, p=p)
+    def start_sentence(self):
+        starts = self.first_state_prob['state']
+        p = normalize(np.array(self.first_state_prob['p']))
+        return np.random.choice(starts, p=p)
+
+    def next_word(self, last_state):
+        ngrams = [ngram for ngram in self.transitions
+                  if ' '.join(ngram.split()[:-1]) == last_state]
+        next = [ngram.split()[-1] for ngram in ngrams]
+        p = normalize(np.array([self.transitions[ngram] for ngram in ngrams]))
+
+        return np.random.choice(next, p=p)
 
     def generate_sentence(self):
-        s = [self.next_word('$')]
+        s = self.start_sentence()
+        next = self.next_word(s)
 
-        while s[-1] != '$':
-            s.append(self.next_word(s[-1]))
+        while next != '$':
+            s += ' ' + next
+            last = last_state(s, self.order)
+            next = self.next_word(last)
 
-        return ' '.join(s[:-1])
+        return ' '.join(s.split()[1:])
+
+
+def normalize(p):
+    if p.sum() < 1:
+        p /= p.sum()
+    return p
+
+
+def last_state(sentence, order):
+    word_list = sentence.split()
+    return ' '.join(word_list[-order:])
